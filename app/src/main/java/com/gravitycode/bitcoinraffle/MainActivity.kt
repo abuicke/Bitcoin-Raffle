d751e4b25891da0e681b3035adc1565b779e4089
Adam Buicke <buickea@gmail.com>
2022-03-22 17:40:57 +0000

    basic primary UI
    1) without Ready button
    2) no functionality
    3) not properly architected

--- app/src/main/java/com/gravitycode/bitcoinraffle/MainActivity.kt
+++ app/src/main/java/com/gravitycode/bitcoinraffle/MainActivity.kt
@@ -2,10 +2,72 @@ package com.gravitycode.bitcoinraffle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.gravitycode.bitcoinraffle.databinding.ActivityMainBinding
+import com.squareup.picasso.Picasso
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        val adapter = UserViewHolderAdapter()
+        adapter.users.add(User("https://i.imgur.com/K72XGlf.png",
+            getString(R.string.test_btc_wallet_address)))
+        adapter.users.add(User("https://i.imgur.com/K72XGlf.png",
+            getString(R.string.test_btc_wallet_address)))
+        adapter.users.add(User("https://i.imgur.com/K72XGlf.png",
+            getString(R.string.test_btc_wallet_address)))
+        adapter.users.add(User("https://i.imgur.com/K72XGlf.png",
+            getString(R.string.test_btc_wallet_address)))
+        adapter.users.add(User("https://i.imgur.com/K72XGlf.png",
+            getString(R.string.test_btc_wallet_address)))
+        adapter.users.add(User("https://i.imgur.com/K72XGlf.png",
+            getString(R.string.test_btc_wallet_address)))
+        adapter.users.add(User("https://i.imgur.com/K72XGlf.png",
+            getString(R.string.test_btc_wallet_address)))
+        adapter.users.add(User("https://i.imgur.com/K72XGlf.png",
+            getString(R.string.test_btc_wallet_address)))
+        adapter.users.add(User("https://i.imgur.com/K72XGlf.png",
+            getString(R.string.test_btc_wallet_address)))
+        adapter.users.add(User("https://i.imgur.com/K72XGlf.png",
+            getString(R.string.test_btc_wallet_address)))
+        adapter.users.add(User("https://i.imgur.com/K72XGlf.png",
+            getString(R.string.test_btc_wallet_address)))
+        adapter.users.add(User("https://i.imgur.com/K72XGlf.png",
+            getString(R.string.test_btc_wallet_address)))
+        adapter.users.add(User("https://i.imgur.com/K72XGlf.png",
+            getString(R.string.test_btc_wallet_address)))
+        binding.recyclerViewUsers.adapter = adapter
+        binding.recyclerViewUsers.layoutManager = LinearLayoutManager(this.baseContext)
     }
+}
+
+data class User(val profilePicUrl: String, val btcWalletAddress: String)
+
+class UserViewHolderAdapter() : RecyclerView.Adapter<UserViewHolder>() {
+
+    val users: ArrayList<User> = ArrayList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserViewHolder {
+        val userListItem = LayoutInflater.from(parent.context)
+            .inflate(R.layout.list_item_user, parent, false)
+        return UserViewHolder(userListItem)
+    }
+
+    override fun onBindViewHolder(holder: UserViewHolder, position: Int) {
+        val user = users[position]
+        Picasso.get()
+            .load(user.profilePicUrl)
+            .into(holder.profilePic)
+        holder.btcAddress.text = user.btcWalletAddress
+    }
+
+    override fun getItemCount() = users.size
 }
