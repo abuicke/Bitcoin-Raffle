232c7c6cef8edc8df90f38e1a3cd0ef4b4ab9177
Adam Buicke <buickea@gmail.com>
2022-04-10 04:57:33 +0100

    Added docs to Bitcoin.NetworkType constants

--- app/src/main/java/com/gravitycode/bitcoinraffle/bitcoin/Bitcoin.kt
+++ app/src/main/java/com/gravitycode/bitcoinraffle/bitcoin/Bitcoin.kt
@@ -13,15 +13,33 @@ import timber.log.Timber
 object Bitcoin {
 
     enum class NetworkType {
+        /**
+         * The main or “production” network where people buy and sell things.
+         * */
         MAIN {
             override fun getNetworkParameters(): NetworkParameters = MainNetParams.get()
         },
+
+        /**
+         * Regression test mode, which is not a public network and requires
+         * you to run a bitcoin daemon with the -regtest flag yourself.
+         * */
         REG {
             override fun getNetworkParameters(): NetworkParameters = RegTestParams.get()
         },
+
+        /**
+         * The public test network (testnet) which is reset from time
+         * to time and exists for us to play about with new features.
+         * */
         TEST {
             override fun getNetworkParameters(): NetworkParameters = TestNet3Params.get()
         },
+
+        /**
+         * Network parameters used by the bitcoinj unit tests (and potentially your own). This
+         * lets you solve a block using Block.solve() by setting difficulty to the easiest possible.
+         * */
         UNIT_TEST {
             override fun getNetworkParameters(): NetworkParameters = UnitTestParams.get()
         };
