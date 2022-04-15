73608798854c591b0355ec6fd11c1b680b62c890
Adam Buicke <buickea@gmail.com>
2022-04-15 03:55:32 +0100

    added todos to top of MainActivity.kt file

--- app/src/main/java/com/gravitycode/bitcoinraffle/app/MainActivity.kt
+++ app/src/main/java/com/gravitycode/bitcoinraffle/app/MainActivity.kt
@@ -23,6 +23,31 @@ import timber.log.Timber
 import javax.inject.Inject
 
 /**
+ * desktop wallet (testnet)     2MuUL489qeTWB9KSqbWnVLP1oU2V5AUGwHa
+ * phone wallet (testnet)       tb1qhg2xc5mvx6ug339yj4pp532ktyfhdmt7ufhel3
+ * faucet return address        mv4rnyY3Su5gjcDNzbMLKBQkBicCtHUtFB
+ *
+ * TODO: Text Fields and Enter Button should move up when keyboard displayed.
+ * TODO: Introduce Abstract IView parent to share boiler plate between View classes.
+ * TODO: Add enter button on keyboard for login.
+ * TODO: Add Sign Out and Settings options in ActionBar Overflow Menu.
+ * TODO: Bitcoin Wallet Login Field should keep going horizontally, not wrap.
+ * TODO: Center text in error message toast.
+ * TODO: Shouldn't be using Throwable message as Toast text in LOGIN_FAILED handling.
+ * TODO: Write Unit Tests For Login View and Associated Functions.
+ * TODO: Put Login Screen in ScrollView (doesn't show wallet field when in landscape orientation).
+ * TODO: Add check for google play services https://developers.google.com/android/reference/com/google/android/gms/common/GoogleApiAvailability#isGooglePlayServicesAvailable(android.content.Context)
+ * TODO: Add warning popup at the beginning of the app with the same warning as bitcoinj that the
+ * risk is entirely on the user. This app is for entertainment purposes only. Only raffle small amounts.
+ * TODO: Add Timber.bug(String) if possible. To create an easy call when adding logs to track a
+ * particular bug which will be removed once the bug is found. TAG = "bug"
+ * TODO: Can unnecessary parts of libraries be stripped out for the APK? Proguard?
+ * TODO: Attach a fee from each raffle https://bitcoinj.org/getting-started-java#customizing-the-sending-process-and-setting-fees
+ * The smallest amount possible. Something like 1 cent.
+ * TODO: Make sure raffle stake is kept below 0.0009 BTC Use TextWatcher and prevent editing prefilled 0.000?
+ * TODO: Raffle View should scroll on landscape orientation (is there a way to do this without scrollview)
+ * TODO: Show BTC price in local currency when typing stake
+ * TODO: Make sure back button functions correctly
  * TODO: Since ACCESS_FINE_LOCATION, BLUETOOTH_ADVERTISE, BLUETOOTH_CONNECT, BLUETOOTH_SCAN and
  * READ_EXTERNAL_STORAGE are considered to be dangerous system permissions, in addition to
  * adding them to your manifest, you must request these permissions at runtime, as described
